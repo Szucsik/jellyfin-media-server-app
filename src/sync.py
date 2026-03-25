78489f8b8075afc5517ed2c9f19be06908af9b69
Your Name <istvan.szucsik@proton.me>
2026-03-25 23:01:11 +0100

    Sync debug

--- src/sync.py
+++ src/sync.py
@@ -110,7 +110,9 @@ class TorrentSyncService:
             return
 
         for symlink_str, original_file in zip(symlink_paths, original_files):
-            save_path.replace(self.config.downloaded_directory, self.config.downloaded_target_directory)
+            self.logger.info("Original save path %s", save_path)
+            save_path = save_path.replace(self.config.downloaded_directory, self.config.downloaded_target_directory)
+            self.logger.info("Containerized save path %s", save_path)
             downloaded_file = Path(save_path) / original_file
             symlink = Path(symlink_str)
 
