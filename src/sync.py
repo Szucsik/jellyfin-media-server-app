2b8640bae4ed90f6231d02247739548f27743f4f
Your Name <istvan.szucsik@proton.me>
2026-04-18 21:32:58 +0200

    bugfix

--- src/sync.py
+++ src/sync.py
@@ -112,13 +112,12 @@ class TorrentSyncService:
 
         for symlink_str, original_file in zip(symlink_paths, original_files):
             self.logger.info("Original save path %s", save_path)
-            original_downloaded_file = Path(save_path) / original_file
             save_path = save_path.replace(self.config.downloaded_directory, self.config.downloaded_target_directory)
             self.logger.info("Containerized save path %s", save_path)
             downloaded_file = Path(save_path) / original_file
             symlink = Path(symlink_str)
 
-            if not original_downloaded_file.exists():
+            if not downloaded_file.exists():
                 self.logger.warning("Downloaded file not found: %s", original_downloaded_file)
                 continue
 
