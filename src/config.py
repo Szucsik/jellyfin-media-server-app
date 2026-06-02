049631939c8112bb960ce1fa9ef7fd28168ffb8b
Szucsik István <istvan.szucsik@lhsystems.com>
2026-06-02 15:28:01 +0200

    Removed notebooks and changed log level back to INFO

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@ class Configuration:
 
     # Init logger
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
         handlers=[
